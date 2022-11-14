283bdb7cfbcfe95eb5c59ea2a078f33dd4428f22
caleb.webster@my.jcu.edu.au <calebwebsteredge@gmail.com>
2022-11-15 01:06:30 +1000

    Complete tests for battleship game

--- spec/battleship/game/game_spec.clj
+++ spec/battleship/game/game_spec.clj
@@ -4,16 +4,120 @@
             [battleship.core.board :as b]
             ))
 
-(describe "Battleship Game Interface"
-          (it "should be able to create a new game"
-              (let [game (start-game 1)
-                    p1-board (:board1 game)
-                    p2-board (:board2 game)]
-                (should= 1 (:size p1-board))
-                (should= 1 (:size p2-board))
-                (should-not-be-nil p1-board)
-                (should-not-be-nil p2-board)
-                (should (b/is-board-clear? p1-board))
-                (should (b/is-board-clear? p2-board))
+;different time zones?
+;complete stage?
+;winning the game
+;cannot keep playing if game is won
+;forfeit the game?
+
+(defn get-current-year [] (.getYear (.atZone (java.time.Instant/now) (java.time.ZoneId/systemDefault))))
+(defn get-current-month [] (.getMonth (.atZone (java.time.Instant/now) (java.time.ZoneId/systemDefault))))
+(defn get-current-day [] (.getDayOfWeek (.atZone (java.time.Instant/now) (java.time.ZoneId/systemDefault))))
+(defn get-current-hour [] (.getHour (.atZone (java.time.Instant/now) (java.time.ZoneId/systemDefault))))
+(defn get-current-minute [] (.getMinute (.atZone (java.time.Instant/now) (java.time.ZoneId/systemDefault))))
+(defn get-current-second [] (.getSecond (.atZone (java.time.Instant/now) (java.time.ZoneId/systemDefault))))
+
+(describe "Battleship Game"
+          (it "should be able to start a new game"
+              (let [game (create-game "p1" "p2" 1 1)
+                    board1 (:board (:player1 game))
+                    board2 (:board (:player2 game))]
+                (should= "p1" (:name (:player1 game)))
+                (should= "p2" (:name (:player2 game)))
+                (should-be-nil (:winner game))
+                (should= 1 (:size board1))
+                (should= 1 (:size board2))
+                (should= :planning (:phase game))
+                (should= :player1 (:whose-turn game))
+                (should= 1 (:num-ships game))
+                (should-be-nil (:winner game))
+                (should (b/is-board-clear? board1))
+                (should (b/is-board-clear? board2))
+                (should= (get-current-year) (get-start-year game))
+                (should= (get-current-month) (get-start-month game))
+                (should= (get-current-day) (get-start-day game))
+                (should= (get-current-hour) (get-start-hour game))
+                (should= (get-current-minute) (get-start-minute game))
+                (should= (get-current-second) (get-start-second game))
+                (should-be-nil (:end-time game))
                 ))
+          (it "should allow both players to place ships in turns"
+              (let [game (-> (create-game "p1" "p2" 2 2)
+                             (player-deploys-ship :player1 #{[0 0]})
+                             (player-deploys-ship :player2 #{[0 0]})
+                             (player-deploys-ship :player1 #{[0 1]})
+                             (player-deploys-ship :player2 #{[0 1]}))]
+                (should (b/is-ship-here? [0 0] (get-board game :player1)))
+                (should (b/is-ship-here? [0 0] (get-board game :player2)))
+                (should (b/is-ship-here? [0 1] (get-board game :player1)))
+                (should (b/is-ship-here? [0 1] (get-board game :player2)))
+                ))
+          (it "should not allow player2 to take the first turn"
+              (let [game (create-game "p1" "p2" 1 1)]
+                (should-throw RuntimeException "Cannot perform action; it is not p2's turn"
+                              (player-deploys-ship game :player2 #{[0 0]}))
+                ))
+          (it "should not allow a player to take a turn twice in a row"
+              (let [game (-> (create-game "p1" "p2" 1 1)
+                             (player-deploys-ship :player1 #{[0 0]})
+                             (player-deploys-ship :player2 #{[0 0]})
+                             )]
+                (should-throw RuntimeException "Cannot perform action; it is not p2's turn"
+                              (player-shoots-cell game :player2 [0 0]))
+                ))
+          (it "should move to attack stage when enough ships are placed"
+              (let [game (-> (create-game "p1" "p2" 1 1)
+                             (player-deploys-ship :player1 #{[0 0]})
+                             (player-deploys-ship :player2 #{[0 0]}))]
+                (should= :attack (:phase game))
+                ))
+          (it "should not allow players to place ships during attack phase"
+              (let [game (-> (create-game "p1" "p2" 2 1)
+                             (player-deploys-ship :player1 #{[0 0]})
+                             (player-deploys-ship :player2 #{[0 0]}))]
+                (should-throw RuntimeException "Cannot place ship during attack phase"
+                              (player-deploys-ship game :player1 #{[1 0]}))
+                ))
+          (it "should allow both players to shoot cells in turns"
+              (let [game (-> (create-game "p1" "p2" 2 1)
+                             (player-deploys-ship :player1 #{[0 0] [1 0]})
+                             (player-deploys-ship :player2 #{[0 1] [1 1]})
+                             (player-shoots-cell :player1 [0 0])
+                             (player-shoots-cell :player2 [0 1])
+                             (player-shoots-cell :player1 [1 0])
+                             (player-shoots-cell :player2 [1 1]))]
+                (should (b/is-cell-shot? [0 0] (get-board game :player2)))
+                (should (b/is-cell-shot? [0 1] (get-board game :player1)))
+                (should (b/is-cell-shot? [1 0] (get-board game :player2)))
+                (should (b/is-cell-shot? [1 1] (get-board game :player1)))
+                ))
+          (it "should not allow players to shoot cells during planning phase"
+              (let [game (create-game "p1" "p2" 1 1)]
+                (should-throw RuntimeException "Cannot shoot cell during planning phase"
+                              (player-shoots-cell game :player1 [0 0]))
+                ))
+          (it "should store the winner when the game is over"
+              (let [game (-> (create-game "p1" "p2" 2 2)
+                             (player-deploys-ship :player1 #{[0 0]})
+                             (player-deploys-ship :player2 #{[0 0]})
+                             (player-deploys-ship :player1 #{[1 0]})
+                             (player-deploys-ship :player2 #{[1 0]})
+                             (player-shoots-cell :player1 [0 0])
+                             (player-shoots-cell :player2 [0 0])
+                             (player-shoots-cell :player1 [1 0]))]
+                (should (b/are-all-ships-sunk? (get-board game :player2)))
+                (should= :player1 (get-winner game))
+                ))
+          (it "should not allow players to take turns when the game is over"
+              (let [game (-> (create-game "p1" "p2" 2 2)
+                             (player-deploys-ship :player1 #{[0 0]})
+                             (player-deploys-ship :player2 #{[0 0]})
+                             (player-deploys-ship :player1 #{[1 0]})
+                             (player-deploys-ship :player2 #{[1 0]})
+                             (player-shoots-cell :player1 [0 0])
+                             (player-shoots-cell :player2 [0 0])
+                             (player-shoots-cell :player1 [1 0]))]
+                (should-throw RuntimeException "Cannot perform action; the game is over"
+                              (player-shoots-cell game :player2 [1 0]))
+              ))
           )
--- src/battleship/game/game.clj
+++ src/battleship/game/game.clj
@@ -1,3 +1,130 @@
-(ns battleship.game.game)
+(ns battleship.game.game
+  (:require [battleship.core.board :as b])
+  )
 
-(defn start-game [board-size] {:board1 {:size 1} :board2 {:size 1}})
+(defn create-game [player1 player2 board-size num-ships]
+  {
+   :player1    {:name player1 :board (b/create-board board-size)}
+   :player2    {:name player2 :board (b/create-board board-size)}
+   :start-time (java.time.Instant/now)
+   :phase      :planning
+   :whose-turn :player1
+   :num-ships  num-ships
+   }
+  )
+
+(defn get-player [game player-key] (player-key game))
+
+(defn get-board [game player-key] (:board (get-player game player-key)))
+
+(defn is-players-turn? [game player-key] (= player-key (:whose-turn game)))
+
+(defn get-opponent-key [player-key] (if (= :player1 player-key) :player2 :player1))
+
+(defn get-opponent [game player-key] (get-player game (get-opponent-key player-key)))
+
+(defn get-opponent-board [game player-key] (:board (get-opponent game player-key)))
+
+(defn whose-turn-next? [game] (if (is-players-turn? game :player1) :player2 :player1))
+
+(defn throw-not-players-turn-exception [game player-key] (throw (RuntimeException. (str "Cannot perform action; it is not " (:name (player-key game)) "'s turn"))))
+
+(defn throw-cannot-place-ship-during-attack-phase-exception [] (throw (RuntimeException. "Cannot place ship during attack phase")))
+
+(defn throw-cannot-shoot-cell-during-planning-phase-exception [] (throw (RuntimeException. "Cannot shoot cell during planning phase")))
+
+(defn throw-game-is-over-exception [] (throw (RuntimeException. "Cannot perform action; the game is over")))
+
+(defn perform-players-action [game player-key action]
+  (action game player-key)
+  )
+
+(defn change-player [game]
+  (assoc game :whose-turn (whose-turn-next? game))
+  )
+
+(defn get-winner [game]
+  (if (b/are-all-ships-sunk? (get-board game :player1))
+    :player2
+    (if (b/are-all-ships-sunk? (get-board game :player2))
+      :player1
+      nil
+      )
+    ))
+
+(defn is-game-over? [game] (not (nil? (get-winner game))))
+
+(defn perform-players-action-if-players-turn [game player-key action]
+  (if (is-players-turn? game player-key)
+    (perform-players-action game player-key action)
+    (throw-not-players-turn-exception game player-key))
+  )
+
+(defn perform-players-action-if-game-not-over [game player-key action]
+  (if (not (is-game-over? game))
+    (perform-players-action-if-players-turn game player-key action)
+    (throw-game-is-over-exception)
+    ))
+
+(defn perform-players-action-and-change-player [game player-key action]
+  (change-player (perform-players-action-if-game-not-over game player-key action))
+  )
+
+(defn create-ship-deploy-action [ship]
+  (fn [game player-key] (assoc game player-key (assoc (get-player game player-key) :board (b/deploy-ship ship (get-board game player-key)))))
+  )
+
+(defn create-shoot-cell-action [x-y-pair]
+  (fn [game player-key] (assoc game (get-opponent-key player-key) (assoc (get-opponent game player-key) :board (b/shoot-cell x-y-pair (get-opponent-board game player-key)))))
+  )
+
+(defn has-player-placed-all-ships? [game player-key] (= (:num-ships game) (b/count-ships (get-board game player-key))))
+
+(defn is-planning-phase-over? [game] (and (is-players-turn? game :player1) (has-player-placed-all-ships? game :player1)))
+
+(defn is-in-attack-phase? [game] (= :attack (:phase game)))
+
+(defn is-in-planning-phase? [game] (= :planning (:phase game)))
+
+(defn switch-to-attack-phase [game] (assoc game :phase :attack))
+
+(defn set-winner [game winner] (assoc game :winner winner))
+
+(defn update-game-phase [game]
+  (if (is-planning-phase-over? game)
+    (switch-to-attack-phase game)
+    game
+    ))
+
+(defn update-game-state [game]
+  (if (is-game-over? game)
+    (set-winner game (get-winner game))
+    (update-game-phase game)
+    ))
+
+(defn perform-players-action-and-update-game [game player-key action]
+  (update-game-state (perform-players-action-and-change-player game player-key action))
+  )
+
+(defn player-deploys-ship [game player-key ship]
+  (let [action (create-ship-deploy-action ship)]
+    (if (is-in-planning-phase? game)
+      (perform-players-action-and-update-game game player-key action)
+      (throw-cannot-place-ship-during-attack-phase-exception)
+      ))
+  )
+
+(defn player-shoots-cell [game player-key x-y-pair]
+  (let [action (create-shoot-cell-action x-y-pair)]
+    (if (is-in-attack-phase? game)
+      (perform-players-action-and-update-game game player-key action)
+      (throw-cannot-shoot-cell-during-planning-phase-exception)
+      )))
+
+(defn get-start-year [game] (.getYear (.atZone (:start-time game) (java.time.ZoneId/systemDefault))))
+(defn get-start-month [game] (.getMonth (.atZone (:start-time game) (java.time.ZoneId/systemDefault))))
+(defn get-start-day [game] (.getDayOfWeek (.atZone (:start-time game) (java.time.ZoneId/systemDefault))))
+(defn get-start-hour [game] (.getHour (.atZone (:start-time game) (java.time.ZoneId/systemDefault))))
+(defn get-start-minute [game] (.getMinute (.atZone (:start-time game) (java.time.ZoneId/systemDefault))))
+(defn get-start-second [game] (.getSecond (.atZone (:start-time game) (java.time.ZoneId/systemDefault))))
+
